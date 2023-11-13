6b7daf4f5d241ddf4ae29e39d6c3dd047d22a89d
Yair Marin <edgar.marin@axity.com>
2023-11-12 22:15:49 -0600

    add letters

--- src/pages/dedications/constants/constants.ts
+++ src/pages/dedications/constants/constants.ts
@@ -9,15 +9,21 @@ export const DEDICATIONS: IDedication[] = [
         text: `Ya son 18 años que no solo son años, sino que es el tiempo que me han acompañado a lo largo de la vida. En el caso de mi niñez, como cuando
         estaba aprendiendo a caminar y cada que me caía ustedes estuvieron para mi para levantarme, cuando entré a la escuela en el caso del kinder,
         la primaria, secundaria y prepa; celebraron cada uno de mis logros, me motivaban para ser mejor persona. Estuvieron en mis mejores momentos y más 
-        ahora que ya estoy en una nueva y bonita etapa apra lograr mi meta, soy feliz y muy agradecida por luchas cada día para estar bien. Es un orgullo
+        ahora que ya estoy en una nueva y bonita etapa para lograr mi meta, soy feliz y muy agradecida por luchas cada día para estar bien. Es un orgullo
         para mi tener a mis mamis y mi hermano conmigo celebrando mis 18 años y que espero Dios, sean aun más cumpleaños juntos.`
     },
     {
         to: 'Padre',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, suscipit, necessitatibus reprehenderit adipisci pariatur voluptatum nobis sequi enim, officia officiis numquam optio facilis. Quis nostrum porro sint reiciendis! Repellendus, earum?'
+        text: `Primeramente gracias por celebrar mis 18 años, por estar presente en este día importante para mí, conviviendo y compartiendo
+        mi felicidad rodeada de mi familia. Tengo la dicha de estar contigo papá Victor y con mis abuelitos Herminia y Domingo. 
+        Se que han pasado muchas cosas tantos buenas como malas pero, quiero que sepan que les tengo u gran cariño y también
+        agradacerles el apoyo, el estar ahí para nosotros acompañado con sabios y risas.`
     },
     {
         to: 'Padrinos',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, suscipit, necessitatibus reprehenderit adipisci pariatur voluptatum nobis sequi enim, officia officiis numquam optio facilis. Quis nostrum porro sint reiciendis! Repellendus, earum?'
+        text: `Para empezar, buenas noches padrinos, les escribí esta dedicatoria solo para darles las gracias de ser mis padrinos,
+        desde que era una bebé, mis padres me entregaron ante ustedes a Dios de aceptarme como ahijada, ahora hoy nuevamente
+        mis padrinos José Andrés Hernández López y Erika Jessica Pacheco Hernández me entregaron antes Dios dándole las gracias
+        por haber permitido ya vivir 18 años de vida. Gracias padrinos por ser más que ahijada sino como una hija. Los quiero mucho.`
     }
 ];
